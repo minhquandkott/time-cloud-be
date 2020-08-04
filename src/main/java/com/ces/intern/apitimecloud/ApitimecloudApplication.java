95b1b7b8e4cd85e2b019e5ac476ba6f9250277e4
Quang Son <sontruongquangson@gmail.com>
2020-08-04 20:33:05 +0700

    Setup project API

---
+++ src/main/java/com/ces/intern/apitimecloud/ApitimecloudApplication.java
@@ -0,0 +1,13 @@
+package com.ces.intern.apitimecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitimecloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApitimecloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ces/intern/apitimecloud/ApitimecloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ces.intern.apitimecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApitimecloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
