b20ac65ab30c73e6b56e63f4b944c1a913140bc8
Quang Son <sontruongquangson@gmail.com>
2020-08-04 20:53:05 +0700

    Create user data transfer object

---
+++ src/main/java/com/ces/intern/apitimecloud/dto/UserDTO.java
@@ -0,0 +1,67 @@
+package com.ces.intern.apitimecloud.dto;
+
+public class UserDTO {
+    private Integer id;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String avatar;
+    private boolean isActived;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public boolean isActived() {
+        return isActived;
+    }
+
+    public void setActived(boolean actived) {
+        isActived = actived;
+    }
+}
