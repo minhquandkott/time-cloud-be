28a712ce9a6656971fc8f8e87f0b1dba23429648
Quang Son <sontruongquangson@gmail.com>
2020-08-13 11:11:19 +0700

    create user infor response

---
+++ src/main/java/com/ces/intern/apitimecloud/http/response/UserResponse.java
@@ -0,0 +1,22 @@
+package com.ces.intern.apitimecloud.http.response;
+
+public class UserResponse {
+    private String password;
+    private String username;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
