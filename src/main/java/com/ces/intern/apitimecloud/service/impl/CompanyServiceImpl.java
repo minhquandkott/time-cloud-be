73521243bfd5dbb74e080cd84b09796db1335b85
Van Hiep <vanhiep99w@gmail.com>
2020-08-19 16:00:05 +0700

    add hashcode equal for embedable class

--- src/main/java/com/ces/intern/apitimecloud/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/ces/intern/apitimecloud/service/impl/CompanyServiceImpl.java
@@ -1,6 +1,14 @@
 package com.ces.intern.apitimecloud.service.impl;
 
-import com.ces.intern.apitimecloud.service.CompanyService;
 
+import com.ces.intern.apitimecloud.service.CompanyService;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class CompanyServiceImpl implements CompanyService {
+
+
+
+
 }
