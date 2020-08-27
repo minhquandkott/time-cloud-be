cf7b381604ea76ff5aa33dae9ca2a93e436c9553
Quang Son <sontruongquangson@gmail.com>
2020-08-27 11:48:03 +0700

    create time repository

--- src/main/java/com/ces/intern/apitimecloud/repository/TimeRepository.java
+++ src/main/java/com/ces/intern/apitimecloud/repository/TimeRepository.java
@@ -1,4 +1,9 @@
 package com.ces.intern.apitimecloud.repository;
 
-public class TimeRepository {
+import com.ces.intern.apitimecloud.entity.TimeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TimeRepository extends JpaRepository<TimeEntity, Integer> {
 }
