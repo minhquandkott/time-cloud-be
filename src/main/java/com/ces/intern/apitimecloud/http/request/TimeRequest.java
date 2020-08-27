9f22441e4fe0e0337e0d32e6d32f13d157c70618
Quang Son <sontruongquangson@gmail.com>
2020-08-27 11:45:45 +0700

    create request and response Time information

--- src/main/java/com/ces/intern/apitimecloud/http/request/TimeRequest.java
+++ src/main/java/com/ces/intern/apitimecloud/http/request/TimeRequest.java
@@ -1,4 +1,51 @@
 package com.ces.intern.apitimecloud.http.request;
 
+import java.util.Date;
+
 public class TimeRequest {
+    private Date startTime;
+    private Date endTime;
+    private String descripion;
+    private Integer userId;
+    private Integer taskId;
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescripion() {
+        return descripion;
+    }
+
+    public void setDescripion(String descripion) {
+        this.descripion = descripion;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(Integer taskId) {
+        this.taskId = taskId;
+    }
 }
