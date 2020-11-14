25f5af2f4099feaea38d0a47952e90091413be9a
Van Hiep <vanhiep99w@gmail.com>
2020-11-15 06:29:40 +0700

    fixbug

--- src/main/java/com/ces/intern/apitimecloud/util/Classifications.java
+++ src/main/java/com/ces/intern/apitimecloud/util/Classifications.java
@@ -1,17 +1,42 @@
 package com.ces.intern.apitimecloud.util;
+import com.ces.intern.apitimecloud.ApplicationContext;
+import io.swagger.models.auth.In;
+import org.omg.PortableInterceptor.INACTIVE;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.ResourceUtils;
 import weka.classifiers.bayes.NaiveBayes;
 import weka.classifiers.meta.FilteredClassifier;
 import weka.core.*;
 
 import java.io.File;
+import java.io.InputStream;
 
 public class Classifications {
 
-    private static String approveModel = new File("").getAbsolutePath()+""+"\\data\\approve.model";
-    private static String bugModel = new File("").getAbsolutePath()+""+"\\data\\bug.model";;
-    private static String featureModel = new File("").getAbsolutePath()+""+"\\data\\feature.model";;
+//    private static String approveModel = new File("").getAbsolutePath()+""+"\\data\\approve.model";
+//    private static String bugModel = new File("").getAbsolutePath()+""+"\\data\\bug.model";;
+//    private static String featureModel = new File("").getAbsolutePath()+""+"\\data\\feature.model";
+    private static InputStream[] getPathModel () {
+        InputStream bug =null, feature=null, approve=null;
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
 
-    private static int Classify(String path,String discussion) throws Exception {
+
+        try {
+
+            bug = classLoader.getResourceAsStream("bug.model");
+            System.out.println(bug);
+            feature = classLoader.getResourceAsStream("feature.model");
+            approve = classLoader.getResourceAsStream("approve.model");
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new InputStream[]{bug, feature, approve};
+    }
+
+    private static int Classify(InputStream path,String discussion) throws Exception {
         FilteredClassifier ft = (FilteredClassifier) SerializationHelper.read(path);
         FastVector fvNominalVal = new FastVector(2);
         fvNominalVal.addElement("0");
@@ -38,7 +63,8 @@ public class Classifications {
     }
 
     public static Integer classifyType(String discussion) throws Exception {
-        String[] listURL = {bugModel,featureModel,approveModel};
+
+        InputStream[] listURL = getPathModel();
         for(int i = 0; i < listURL.length; i++){
             if(Classify(listURL[i],discussion) == 1){
                 return i;
