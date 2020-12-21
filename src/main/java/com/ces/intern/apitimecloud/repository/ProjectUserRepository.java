3816a29c8cd3bf49502022a233a3d5414a85bc4e
Quan T. M. Huynh <=>
2020-12-21 17:58:17 +0700

    f

--- src/main/java/com/ces/intern/apitimecloud/repository/ProjectUserRepository.java
+++ src/main/java/com/ces/intern/apitimecloud/repository/ProjectUserRepository.java
@@ -24,8 +24,7 @@ public interface ProjectUserRepository extends JpaRepository<ProjectUserEntity, 
         @Query(value = "update project_user set is_doing = false where project_id = :projectId", nativeQuery = true)
         void deleteProjectById(@Param("projectId") Integer projectId);
 
-        @Query(value = "select project_user.project_id,user_id,is_doing,index,is_show from project_user join project on project_user.project_id = project.project_id\n" +
-                "where user_id = :userId and done = false;", nativeQuery = true)
+        @Query(value = "select * from project_user where is_doing = true and user_id = :userId and project_id in (select project_id from project where  done = false)", nativeQuery = true)
         List<ProjectUserEntity> getAllByUserIdAndNotDone(@Param("userId") Integer userId);
 
 
